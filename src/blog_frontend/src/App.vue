a20e3232c5bbc14e3932c6322ba6f99ce1b78547
Wredi <piotr.skora25@gmail.com>
2024-12-04 16:50:22 +0100

    added basic frontend

--- src/blog_frontend/src/App.vue
+++ src/blog_frontend/src/App.vue
@@ -6,23 +6,27 @@ let greeting = ref('');
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await blog_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+
+  const title = target.querySelector('#title').value;
+  const content = target.querySelector('#content').value;
+  const tags = target.querySelector('#tags').value;
+  const splitedTags = tags.split(",");
+
+  await blog_backend.add_blog(title, content, splitedTags);
 }
 </script>
 
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
-  </main>
+    <main>
+        <img src="/logo2.svg" alt="DFINITY logo" />
+        <br />
+        <br />
+        <form action="#" @submit="handleSubmit">
+            <div><p>Title: </p><input id="title" alt="title" type="text" /></div>
+            <div><p>Content: </p><input id="content" alt="content" type="text" /></div>
+            <div><p>Tags: </p><input id="tags" alt="tags" type="text" /></div>
+            <button type="submit">Click to add!</button>
+        </form>
+        <section id="greeting">{{ greeting }}</section>
+    </main>
 </template>
